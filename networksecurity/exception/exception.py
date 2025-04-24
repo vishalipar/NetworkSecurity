de27ba875e33f2dad79688c726b5fb0a4a0c8853
vishalipar <vishalipar04@gmail.com>
2025-04-24 23:05:02 +0530

    Add phishingData.csv file in Network Data folder

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -9,7 +9,7 @@ class NetworkSecurityException(Exception):
         self.lineno = exc_tb.tb_lineno
         self.file_name=exc_tb.tb_frame.f_code.co_filename
         
-    def _str__(self):
+    def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
             self.file_name, self.lineno, str(self.error_message))
             
